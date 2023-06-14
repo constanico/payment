a49cb2d1ac5cb6f926607edb8b4b6e687afb1996
BERNICO CONSTANTINO <U549169@bca.co.id>
2023-06-14 15:21:34 +0700

    Payment

--- src/main/kotlin/com/bca/payment/service/PaymentService.kt
+++ src/main/kotlin/com/bca/payment/service/PaymentService.kt
@@ -2,7 +2,6 @@ package com.bca.payment.service
 
 import com.bca.payment.model.Transaction
 import com.bca.payment.model.TransactionList
-import org.springframework.http.ResponseEntity
 
 interface PaymentService {
     fun getPayment(): TransactionList
--- src/main/kotlin/com/bca/payment/service/PaymentServiceImpl.kt
+++ src/main/kotlin/com/bca/payment/service/PaymentServiceImpl.kt
@@ -2,12 +2,10 @@ package com.bca.payment.service
 
 import com.bca.payment.model.Transaction
 import com.bca.payment.model.TransactionList
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import org.springframework.web.client.RestTemplate
 
 @Service
-class PaymentServiceImpl(@Autowired private val restTemplate: RestTemplate): PaymentService {
+class PaymentServiceImpl(): PaymentService {
     val transactionList = TransactionList()
 
     override fun getPayment(): TransactionList {
